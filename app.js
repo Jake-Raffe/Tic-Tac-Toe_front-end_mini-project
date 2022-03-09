1da19f0679095fb78c80463efca2fd5884ba467c
Jake Raffe <jakeraffe24@gmail.com>
2022-03-09 17:40:56 +0000

    Created template grid and elements for each square. Adds an 'X' when a square is clicked

---
+++ app.js
@@ -0,0 +1,101 @@
+
+// add X to div box when clicked
+
+    // boxTopLeft addTopLeft
+        // locate div section
+        const boxTopLeft = document.querySelector("#top-left");
+        // create function that adds a 'X' inner text to div
+        const addTopLeft = () => {
+            console.log('clicked');
+            boxTopLeft.innerText = 'X';
+        }
+        // create event listener for clicking on div -> run add x function
+        boxTopLeft.addEventListener('click', addTopLeft);
+
+    // boxTopMid addTopMid
+        // locate div section
+        const boxTopMid = document.querySelector("#top-mid");
+        // create function that adds a 'X' inner text to div
+        const addTopMid = () => {
+            console.log('clicked');
+            boxTopMid.innerText = 'X';
+        }
+        // create event listener for clicking on div -> run add x function
+        boxTopMid.addEventListener('click', addTopMid);
+
+    // boxTopRight addTopRight
+        // locate div section
+        const boxTopRight = document.querySelector("#top-right");
+        // create function that adds a 'X' inner text to div
+        const addTopRight = () => {
+            console.log('clicked');
+            boxTopRight.innerText = 'X';
+        }
+        // create event listener for clicking on div -> run add x function
+        boxTopRight.addEventListener('click', addTopRight);
+
+    // boxMidLeft addMidLeft
+        // locate div section
+        const boxMidLeft = document.querySelector("#mid-left");
+        // create function that adds a 'X' inner text to div
+        const addMidLeft = () => {
+            console.log('clicked');
+            boxMidLeft.innerText = 'X';
+        }
+        // create event listener for clicking on div -> run add x function
+        boxMidLeft.addEventListener('click', addMidLeft);
+
+    // boxMidMid addMidMid
+        // locate div section
+        const boxMidMid = document.querySelector("#mid-mid");
+        // create function that adds a 'X' inner text to div
+        const addMidMid = () => {
+            console.log('clicked');
+            boxMidMid.innerText = 'X';
+        }
+        // create event listener for clicking on div -> run add x function
+        boxMidMid.addEventListener('click', addMidMid);
+
+    // boxMidRight addMidRight
+        // locate div section
+        const boxMidRight = document.querySelector("#mid-right");
+        // create function that adds a 'X' inner text to div
+        const addMidRight = () => {
+            console.log('clicked');
+            boxMidRight.innerText = 'X';
+        }
+        // create event listener for clicking on div -> run add x function
+        boxMidRight.addEventListener('click', addMidRight);
+
+    // boxBottomLeft addBottomLeft
+        // locate div section
+        const boxBottomLeft = document.querySelector("#bottom-left");
+        // create function that adds a 'X' inner text to div
+        const addBottomLeft = () => {
+            console.log('clicked');
+            boxBottomLeft.innerText = 'X';
+        }
+        // create event listener for clicking on div -> run add x function
+        boxBottomLeft.addEventListener('click', addBottomLeft);
+
+    // boxBottomMid addBottomMid
+        // locate div section
+        const boxBottomMid = document.querySelector("#bottom-mid");
+        // create function that adds a 'X' inner text to div
+        const addBottomMid = () => {
+            console.log('clicked');
+            boxBottomMid.innerText = 'X';
+        }
+        // create event listener for clicking on div -> run add x function
+        boxBottomMid.addEventListener('click', addBottomMid);
+
+    // boxBottomRight addBottomRight
+        // locate div section
+        const boxBottomRight = document.querySelector("#bottom-right");
+        // create function that adds a 'X' inner text to div
+        const addBottomRight = () => {
+            console.log('clicked');
+            boxBottomRight.innerText = 'X';
+        }
+        // create event listener for clicking on div -> run add x function
+        boxBottomRight.addEventListener('click', addBottomRight);
