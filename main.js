14d9bdada0e472d864d282520bce63ef2865afab
Jake Raffe <jakeraffe24@gmail.com>
2022-03-10 14:01:31 +0000

    made gameWon for all possibilities. Made game won alert. Can only click on a box once.

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 // import classes 
 // const { Player } = require("./player");
+
+// Player constructor
 const Player = function(name, icon){
     this.name = name,
     this.icon = icon
@@ -17,10 +19,8 @@ let currentPlayer = player1
 // alternate output depending on player
 const altSymbol = (player) => {
     if (player.name === 'Player 1'){
-        console.log('player 1');
         return 'X';
     } else if (player.name === 'Player 2'){
-        console.log('player 2');
         return 'O';
     } else {
         console.log("Error with determining player number");
@@ -29,13 +29,21 @@ const altSymbol = (player) => {
 
 // change player function
 const changePlayer = (player) => {
-    if (currentPlayer === player1){
-        console.log('changing to player 2');
+    if (player === player1){
         currentPlayer = player2;
-    } else if (currentPlayer === player2){
-        console.log('changing to player 3');
+    } else if (player === player2){
         currentPlayer = player1;
     }
+}
+
+// check if game won
+const checkGameWon = () => {
+    if (gameWon(currentPlayer)){
+        // wait for icon to be added to grid before alert message
+        changePlayer(currentPlayer);
+        setTimeout(function(){ alert (currentPlayer.name + ' has won!') }, 50)
+    }
+    
 }
 
 // functions to add X to div box when clicked
@@ -45,9 +53,11 @@ const changePlayer = (player) => {
         const boxTopLeft = document.querySelector("#top-left");
         // create function that adds a 'X' inner text to div
         function addTopLeft() {
+            if (checkBoxEmpty(boxTopLeft)){
             boxTopLeft.innerText = altSymbol(currentPlayer);
-            gameWon(currentPlayer);
+            checkGameWon();
             changePlayer(currentPlayer);
+            }
         }
         // create event listener for clicking on div -> run add x function
         boxTopLeft.addEventListener('click', addTopLeft);
@@ -57,9 +67,11 @@ const changePlayer = (player) => {
         const boxTopMid = document.querySelector("#top-mid");
         // create function that adds a 'X' inner text to div
         const addTopMid = () => {
+            if (checkBoxEmpty(boxTopMid)){
             boxTopMid.innerText = altSymbol(currentPlayer);
-            gameWon(currentPlayer);
+            checkGameWon();
             changePlayer(currentPlayer);
+            }
         }
         // create event listener for clicking on div -> run add x function
         boxTopMid.addEventListener('click', addTopMid);
@@ -69,9 +81,11 @@ const changePlayer = (player) => {
         const boxTopRight = document.querySelector("#top-right");
         // create function that adds a 'X' inner text to div
         const addTopRight = () => {
+            if (checkBoxEmpty(boxTopRight)){
             boxTopRight.innerText = altSymbol(currentPlayer);
-            gameWon(currentPlayer);
+            checkGameWon();
             changePlayer(currentPlayer);
+            }
         }
         // create event listener for clicking on div -> run add x function
         boxTopRight.addEventListener('click', addTopRight);
@@ -81,9 +95,11 @@ const changePlayer = (player) => {
         const boxMidLeft = document.querySelector("#mid-left");
         // create function that adds a 'X' inner text to div
         const addMidLeft = () => {
+            if (checkBoxEmpty(boxMidLeft)){
             boxMidLeft.innerText = altSymbol(currentPlayer);
-            gameWon(currentPlayer);
+            checkGameWon();
             changePlayer(currentPlayer);
+            }
         }
         // create event listener for clicking on div -> run add x function
         boxMidLeft.addEventListener('click', addMidLeft);
@@ -93,10 +109,11 @@ const changePlayer = (player) => {
         const boxMidMid = document.querySelector("#mid-mid");
         // create function that adds a 'X' inner text to div
         const addMidMid = () => {
+            if (checkBoxEmpty(boxMidMid)){
             boxMidMid.innerText = altSymbol(currentPlayer);
-            gameWon(currentPlayer);
+            checkGameWon();
             changePlayer(currentPlayer);
-        }
+        }}
         // create event listener for clicking on div -> run add x function
         boxMidMid.addEventListener('click', addMidMid);
 
@@ -105,9 +122,11 @@ const changePlayer = (player) => {
         const boxMidRight = document.querySelector("#mid-right");
         // create function that adds a 'X' inner text to div
         const addMidRight = () => {
+            if (checkBoxEmpty(boxMidRight)){
             boxMidRight.innerText = altSymbol(currentPlayer);
-            gameWon(currentPlayer);
+            checkGameWon();
             changePlayer(currentPlayer);
+            }
         }
         // create event listener for clicking on div -> run add x function
         boxMidRight.addEventListener('click', addMidRight);
@@ -117,9 +136,11 @@ const changePlayer = (player) => {
         const boxBottomLeft = document.querySelector("#bottom-left");
         // create function that adds a 'X' inner text to div
         const addBottomLeft = () => {
+            if (checkBoxEmpty(boxBottomLeft)){
             boxBottomLeft.innerText = altSymbol(currentPlayer);
-            gameWon(currentPlayer);
+            checkGameWon();
             changePlayer(currentPlayer);
+            }
         }
         // create event listener for clicking on div -> run add x function
         boxBottomLeft.addEventListener('click', addBottomLeft);
@@ -129,9 +150,11 @@ const changePlayer = (player) => {
         const boxBottomMid = document.querySelector("#bottom-mid");
         // create function that adds a 'X' inner text to div
         const addBottomMid = () => {
+            if (checkBoxEmpty(boxBottomMid)){
             boxBottomMid.innerText = altSymbol(currentPlayer);
-            gameWon(currentPlayer);
+            checkGameWon();
             changePlayer(currentPlayer);
+            }
         }
         // create event listener for clicking on div -> run add x function
         boxBottomMid.addEventListener('click', addBottomMid);
@@ -141,13 +164,23 @@ const changePlayer = (player) => {
         const boxBottomRight = document.querySelector("#bottom-right");
         // create function that adds a 'X' inner text to div
         const addBottomRight = () => {
+            if (checkBoxEmpty(boxBottomRight)){
             boxBottomRight.innerText = altSymbol(currentPlayer);
-            gameWon(currentPlayer);
+            checkGameWon();
             changePlayer(currentPlayer);
+            }
         }
         // create event listener for clicking on div -> run add x function
         boxBottomRight.addEventListener('click', addBottomRight);
 
+// check if box is empty before adding icon
+    const checkBoxEmpty = (box) => {
+        if (box.innerText === ''){
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 // Clear Grid
 
@@ -171,10 +204,39 @@ const changePlayer = (player) => {
 // Check if game won
         // will take player number as argument
         function gameWon (player) {
+            // top row
             if (boxTopLeft.innerText === player.icon && boxTopMid.innerText === player.icon && boxTopRight.innerText === player.icon) {
+                //alert(player.name + ' has won');
+                return true;
+                // mid row
+            } else if (boxMidLeft.innerText === player.icon && boxMidMid.innerText === player.icon && boxMidRight.innerText === player.icon) {
+                // alert(player.name + ' has won');
+                return true;
+                // bottom row
+            } else if (boxBottomLeft.innerText === player.icon && boxBottomMid.innerText === player.icon && boxBottomRight.innerText === player.icon) {
                 console.log(player.name + ' has won');
                 return true;
-            } else {
+                // left column
+            } else if (boxTopLeft.innerText === player.icon && boxMidLeft.innerText === player.icon && boxBottomLeft.innerText === player.icon) {
+                console.log(player.name + ' has won');
+                return true;
+                // mid column
+            } else if (boxTopMid.innerText === player.icon && boxMidMid.innerText === player.icon && boxBottomMid.innerText === player.icon) {
+                console.log(player.name + ' has won');
+                return true;
+                // right column
+            } else if (boxTopRight.innerText === player.icon && boxMidRight.innerText === player.icon && boxBottomRight.innerText === player.icon) {
+                console.log(player.name + ' has won');
+                return true;
+                // diag 1
+            } else if (boxTopLeft.innerText === player.icon && boxMidMid.innerText === player.icon && boxBottomRight.innerText === player.icon) {
+                console.log(player.name + ' has won');
+                return true;
+                // diag 2
+            } else if (boxTopRight.innerText === player.icon && boxMidMid.innerText === player.icon && boxBottomLeft.innerText === player.icon) {
+                console.log(player.name + ' has won');
+                return true;
+            } else  {
                 return false;
             }
         }
